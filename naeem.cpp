689e71590acc28a31abcdec9b21f14cf63de8511
Naeem Khoshnevis;nkhshnvs@memphis.edu <nkhshnvs@login1.localdomain>
2015-02-21 15:24:19 -0600

    Progress up to step 10

--- naeem.cpp
+++ naeem.cpp
@@ -16,7 +16,6 @@
  int main() {
      
      cout << "I was able to compile this code using the HPC at the University of Memphis. When I compiled it there, it did not produce any warning message. The HPC uses a GNU C++ compiler that can be considered a good up-to-date standard. I also version-controlled this code using git, and used a remote repository hosted by github. If I can do this, so can you!!!" << "\n";
-  
      cout << "\n";
      cout << "I am so cool, that I was also able to write a code that produces the first M numbers of the Fibonacci sequence. Here they are: " << "\n"; 
  
